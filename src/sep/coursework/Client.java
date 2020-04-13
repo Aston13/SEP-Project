0d28204772d5734afe7ffaecec785a52317c0bc8
Aston13 <Aston_Turner@hotmail.co.uk>
2020-04-13 01:33:47 +0100

    Set up resource bundle class

--- src/sep/coursework/Client.java
+++ src/sep/coursework/Client.java
@@ -1,9 +1,15 @@
 package sep.coursework;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 // Must remain the main class for running the client (via main()).
 public class Client {
+    
+    private static final String RESOURCE_PATH = "resourcebundles/MessageBundle";
+    private final ResourceBundle resourceBundle;
+    private final Locale locale = new Locale("en", "GB");
     
     /* Must accept the same arguments as current. 
      * User name, host name and port number.
@@ -13,6 +19,7 @@ public class Client {
     }
     
     public Client(String user, String host, int port) throws IOException {
+        resourceBundle = ResourceBundle.getBundle(RESOURCE_PATH, locale);
         Model model = new Model(user, host, port);
         View view = new View();
         new Controller(model, view);
